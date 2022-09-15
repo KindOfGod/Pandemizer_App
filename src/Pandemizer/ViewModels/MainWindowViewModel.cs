de4eb63135b5af4feb72bc662deddc289e6808f4
Alexander Lutz <alexander.lutz.4.2014@gmail.com>
2022-09-15 19:45:43 +0200

    Basic Pane Menu

--- src/Pandemizer/ViewModels/MainWindowViewModel.cs
+++ src/Pandemizer/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using ReactiveUI;
+﻿using System;
+using System.Reactive;
+using Material.Icons;
+using ReactiveUI;
 
 namespace Pandemizer.ViewModels
 {
@@ -7,15 +10,29 @@ namespace Pandemizer.ViewModels
         #region Fields
 
         private bool _isPaneOpen;
+
+        private ViewModelBase _navigationContent;
+        private MaterialIconKind _hamburgerMenuIcon;
         
         #endregion
 
         #region Properties
-
         public bool IsPaneOpen
         {
             get => _isPaneOpen;
             set => this.RaiseAndSetIfChanged(ref _isPaneOpen, value);
+        }
+
+        public ViewModelBase NavigationContent
+        {
+            get => _navigationContent;
+            set => this.RaiseAndSetIfChanged(ref _navigationContent, value);
+        }
+
+        public MaterialIconKind HamburgerMenuIcon
+        {
+            get => _hamburgerMenuIcon;
+            set => this.RaiseAndSetIfChanged(ref _hamburgerMenuIcon, value);
         }
 
         #endregion
@@ -25,6 +42,29 @@ namespace Pandemizer.ViewModels
         public MainWindowViewModel()
         {
             IsPaneOpen = true;
+            HamburgerMenuIcon = MaterialIconKind.HamburgerMenuBack;
+
+            NavigationContent = null;
+
+            HamburgerMenuButtonClick = ReactiveCommand.Create(OnHamburgerMenuClick);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ReactiveCommand<Unit, Unit> HamburgerMenuButtonClick { get; }
+
+        #endregion
+
+        #region Private Methods
+
+        private void OnHamburgerMenuClick()
+        {
+            IsPaneOpen = !IsPaneOpen;
+            HamburgerMenuIcon = HamburgerMenuIcon == MaterialIconKind.HamburgerMenu
+                ? MaterialIconKind.HamburgerMenuBack
+                : MaterialIconKind.HamburgerMenu;
         }
 
         #endregion
